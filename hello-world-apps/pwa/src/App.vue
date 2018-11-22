fc21b44efa4ccb2df7d096f64e231973608fae1f
S Grabinski <thegrabinski@gmail.com>
2018-11-21 22:12:29 -0500

    Created a PWA project using vue-cli 3

---
+++ hello-world-apps/pwa/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HelloWorldServices type="Node" domain="localhost:7272" />
+    <HelloWorldServices type="Java" domain="localhost:7070" />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorldServices from './components/HelloWorldServices.vue'
+
+export default {
+  name: 'app',
+  components: { HelloWorldServices }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world-apps/pwa/src/components/HelloWorldServices.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <h2> Hello World Services </h2>
+    <p>This is a hello message service response: </p>
+    <ul>
+        <li>Message from {{ type }} at {{ domain }}</li>
+        <li>Timestamp</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default { name: 'HelloWorldServices', props: { type: String, domain: String } }
+</script>
