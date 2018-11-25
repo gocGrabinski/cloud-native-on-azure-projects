f55a644e987cdc9e629e39d07f5ea6bdbfa60e41
S Grabinski <thegrabinski@gmail.com>
2018-11-25 13:47:45 -0500

    Working compose example.

--- hello-world-apps/webapp/src/index.js
+++ hello-world-apps/webapp/src/index.js
@@ -1,9 +1,14 @@
-var java_el = document.getElementById("java_hello");
-fetchGreetingData(java_el);
+var hello_divs = document.getElementsByClassName("hello");
+console.log(hello_divs);
+Array.prototype.forEach.call(hello_divs, (element) => {
+    fetchGreetingData(element);
+});
 
 
 function fetchGreetingData(element) {
-    fetch('/test.json')
+    var type = element.getAttribute("id").replace("_hello", "");
+    console.log(`/${type}`);
+    fetch(`/${type}`)
         .then(function (response) {
             return response.json();
         }).then(function (myJson) {
