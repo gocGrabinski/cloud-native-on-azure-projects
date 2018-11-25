92e4c5d0a385412cc100a9d145b77cabe1126ea2
S Grabinski <thegrabinski@gmail.com>
2018-11-24 22:09:38 -0500

    Deleted PWA - Updated webapp

--- hello-world-apps/webapp/src/index.js
+++ hello-world-apps/webapp/src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+var java_el = document.getElementById("java_hello");
+fetchGreetingData(java_el);
+
+
+function fetchGreetingData(element) {
+    fetch('/test.json')
+        .then(function (response) {
+            return response.json();
+        }).then(function (myJson) {
+            console.log(myJson.Greeting);
+            element.getElementsByTagName("P")[0].textContent = myJson.Greeting;
+        });
+}
--- hello-world-apps/webapp/webpack.config.js
+++ hello-world-apps/webapp/webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
     module: {
         rules: [
             {
@@ -15,12 +19,17 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/template.html'
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         // compress: true,
         // port: 9000,
         hot: true
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     }
 }
