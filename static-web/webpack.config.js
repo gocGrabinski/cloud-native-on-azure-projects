b11d6dd3d7238815ab56081d1d8bce43de00d3ee
gocGrabinski <stan.grabinski@hrsdc-rhdcc.gc.ca>
2019-01-07 21:39:03 -0500

    Vulnerabilityfix jan7 2019 (#2)
    * Created a PWA project using vue-cli 3
    
    * Added nginx.conf file.
    
    * Added a docker-compose file.
    
    * Uncommented the 404 error page directive
    
    * Added Java app
    
    * Added a simple webapp with webpack - Vue.js not needed for this  demo.
    
    * Deleted PWA - Updated webapp
    
    * Working compose example.
    
    * OnCallApp Prototype
    
    * Updated dependencies.

---
+++ static-web/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new CopyWebpackPlugin([{ from: 'html' }])
+    ]
+};
